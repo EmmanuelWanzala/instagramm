49818c666489e7461fd724fd11a8f109580d44f9
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-10-18 18:38:43 +0300

    remove decouple

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -10,9 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-# import django_heroku
-# from decouple import config, Csv
-# import dj_database_url
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,4 +149,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
