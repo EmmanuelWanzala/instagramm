801e3a3400c718f1cc90556049fb668e527905da
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-10-19 10:55:41 +0300

    changed database

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from pathlib import Path
 import django_heroku
 import dj_database_url
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +83,8 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'moringa',
-        'USER':'second',
+        'NAME': 'second',
+        'USER':'moringa',
         'PASSWORD' : 'Access',
     }
 }
